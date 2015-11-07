55b4ce9f52576ac53861b60f297c2b80570ecbb5
Timo Rongas <timo.rongas@gmail.com>
2015-11-07 15:10:37 +0200

    Single badge details

--- server.js
+++ server.js
@@ -19,6 +19,20 @@ app.get('/badges', function(req, res) {
 
 });
 
+app.get('/badges/:id', function(req, res) {
+    res.send({
+        'id': req.params.id,
+        'name': 'SuperAwesomeBadge1',
+        'icon': 'a.bad.url',
+        'description': 'This is a very cool badge you can show off to your friends',
+        'status': {
+            'earned': true,
+            'earnLocation': 'iHaveNoClueWhatTypeIShouldBe',
+            'earnTs': '2015-04-01T00:34:01+0200'
+        }
+    });
+});
+
 var server = app.listen(8080, '0.0.0.0', function() {
     console.log("Hit me, I am listening at localhost:8080.");
 })
